92022f4449de00c35478b730ad359f69d3188f1e
Alex Dasneves <adasneves127@gmail.com>
2025-04-27 20:12:21 -0400

    Add Operator Management

---
+++ Chronicle/Utils/MenuUtils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace Chronicle.Utils
+{
+    public static class MenuUtils
+    {
+        public static void populateMenu(ToolStripItemCollection parent, string path)
+        {
+
+            using (MySqlConnection conn = new(Globals.ConnectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT DISTINCT A.* FROM MENU_ITEMS A, MENU_ITEM_ACCESS B, OPERATOR_CLASS_LINK C WHERE parentItemID is NULL AND A.menuItemID = B.menuItemID AND B.operatorClassID = C.operatorClassID AND C.operatorID = @operatorID ORDER BY sortOrder DESC";
+                cmd.Parameters.AddWithValue("@operatorID", Globals.OperatorID);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ToolStripDropDownItem item = new NavElement(reader.GetString("menuText"), reader["pluginID"] is DBNull ? null : reader.GetString("pluginID"));
+                    parent.Add(item);
+                    populateMenu(item.DropDownItems, reader.GetInt32("menuItemID"), $"{path}/{reader.GetString("menuText")}");
+                }
+                reader.Close();
+            }
+        }
+
+        public static void populateMenu(ToolStripItemCollection parent, int parentID, string path)
+        {
+            using (MySqlConnection conn = new(Globals.ConnectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT DISTINCT A.* FROM MENU_ITEMS A, MENU_ITEM_ACCESS B, OPERATOR_CLASS_LINK C WHERE parentItemID = @pID AND A.menuItemID = B.menuItemID AND B.operatorClassID = C.operatorClassID AND C.operatorID = @operatorID ORDER BY sortOrder DESC";
+                cmd.Parameters.AddWithValue("@pID", parentID);
+                cmd.Parameters.AddWithValue("@operatorID", Globals.OperatorID);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    ToolStripDropDownItem item = new NavElement(reader.GetString("menuText"), reader["pluginID"] is DBNull ? null : reader.GetString("pluginID"));
+                    parent.Add(item);
+                    populateMenu(item.DropDownItems, reader.GetInt32("menuItemID"), $"{path}/{reader.GetString("menuText")}");
+                }
+                reader.Close();
+            }
+        }
+    }
+}
